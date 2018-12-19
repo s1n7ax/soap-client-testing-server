98854474084179ed1c414efc01056a9d7831b01e
Nisala Muhandiram <Nisala.Muhandiram@infor.com>
2018-12-19 15:47:05 +0530

    add runner executable

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PROJECT_ROOT=$(pwd)
+SOAP="$PROJECT_ROOT/soap"
+RESTful="$PROJECT_ROOT/rest"
+
+cd $SOAP
+javac soapserver\UserModel.java soapserver\UserController.java soapserver\UserControllerImpl.java soapserver\Publisher.java
+java soapserver.Publisher &
+
+cd $RESTful
+npm start
+
