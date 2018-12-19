54fb680c280ec1ddf48f811dd283c04ba7744552
Nisala Muhandiram <Nisala.Muhandiram@infor.com>
2018-12-19 16:14:59 +0530

    change host

--- soap/soapserver/Publisher.java
+++ soap/soapserver/Publisher.java
@@ -4,7 +4,7 @@ import javax.xml.ws.Endpoint;
 
 public class Publisher {
 	public static void main(String[] args) {
-		Endpoint.publish("http://lkcovwkryptsrv1:3001/ws/users", new UserControllerImpl());
+		Endpoint.publish("http://localhost:3001/ws/users", new UserControllerImpl());
 		System.out.println("Soap Server Started");
 	}
 }
